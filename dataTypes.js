287cdec4fe1566abf3ba068f21c0c8c38785aa56
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2023-12-10 19:37:49 +0300

    modified

---
+++ dataTypes.js
@@ -0,0 +1,12 @@
+const num = 5;
+const Name = 'ali';
+const isMajor = false;
+const grade = 4.5;
+const w= undefined;
+const m = null;
+let u;
+
+console.log("my name is " + Name);
+console.log(`my name is ${Name}`);
+console.log(typeof Name);
+console.log(typeof Name === 'string');
