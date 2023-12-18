22f778985286b59d01282d94725cb871cf264cd5
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2023-12-18 11:08:13 +0300

    switch

---
+++ switch.js
@@ -0,0 +1,29 @@
+  //switch
+  let day =8;
+  switch (day){
+      case 1:
+          console.log('sabti'); 
+          break;
+      case 2:
+          console.log('axad'); 
+          break;
+      case 3:
+          console.log('isniin'); 
+          break;
+      case 4:
+          console.log('talaado'); 
+          break;
+      case 5:
+          console.log('arbaco'); 
+          break;
+      case 6:
+          console.log('khamiis'); 
+          break;
+      case 7:
+      case 8:
+          console.log('jimco'); 
+          break;
+      default:
+              console.log('invalid day!');
+              break;
+  }
